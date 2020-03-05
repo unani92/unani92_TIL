ce98a54e8f34deb49f51ae37d404853f092f67de
unani <unani92@naver.com>
2020-03-06 01:38:35 +0900

    binary tree DF traverse

---
+++ programmers/online_lecture/tree/binary_tree.py
@@ -0,0 +1,85 @@
+class Node:
+
+    def __init__(self, item):
+        self.data = item
+        self.left = None
+        self.right = None
+
+
+    def size(self):
+        l = self.left.size() if self.left else 0
+        r = self.right.size() if self.right else 0
+        return l + r + 1
+
+
+    def depth(self):
+        l = self.left.depth() if self.left else 0
+        r = self.right.depth() if self.right else 0
+        return max(l,r)+1
+
+    def inorder(self) : 
+        traversal = []
+        if self.left : 
+            traversal += self.left.inorder()
+        
+        traversal.append(self.data)
+
+        if self.right : 
+            traversal += self.right.inorder()
+
+        return traversal
+
+    def preorder(self) : 
+        traversal = []
+
+        traversal.append(self.data)
+        if self.left : 
+            traversal += self.left.preorder()
+        
+        if self.right : 
+            traversal += self.right.preorder()
+
+        return traversal
+
+class BinaryTree:
+
+    def __init__(self, r):
+        self.root = r
+
+    def size(self):
+        if self.root:
+            return self.root.size()
+        else:
+            return 0
+
+    def depth(self):
+        if self.root : 
+            return self.root.depth()
+        else : 
+            return 1
+
+    def inorder(self) : 
+        if self.root : 
+            return self.root.inorder()
+        else : 
+            return []
+
+    def preorder(self) : 
+        if self.root : 
+            return self.root.preorder()
+        else : 
+            return []
+
+
+N = Node('A')
+N.left = Node('B')
+N.right = Node('C')
+N.left.left = Node('D')
+N.left.right = Node('E')
+N.left.left.left = Node('H')
+N.right.left = Node('F')
+N.right.right = Node('G')
+N.right.left.right = Node('J')
+
+BT = BinaryTree(N)
+print(BT.preorder())
